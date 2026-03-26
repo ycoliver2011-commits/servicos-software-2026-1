9b11749c11949045ca657b22cab377396aa4cf50
Marco Mazzei <marco.mazzei@maua.br>
2026-03-26 20:49:34 -0300

    Criação do backend-json

---
+++ passo-a-passo-whisper_p/backend-json/app/main.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+import whisper 
+from typing import Optional, Union
+from fastapi import FastAPI
+from fastapi import File, UploadFile
+
+import json
+from pydantic import BaseModel
+
+app=FastAPI()
+
+print ("Carregando modelo de IA (Whisper)...")
+model = whisper.load_model("base")
+print("Modelo carregado!")
+
+@app.get("/")
+def diz_ola():
+    return { "Olá" : "Mundo" }
+
+
+@app.post("/transcrever")
+async def transcrever_audio(file: UploadFile = File(...)):
+    caminho_temp = f"temp_{file.filename}"
+    with open(caminho_temp, "wb") as buffer:
+        shutil.copyfileobj(file.file,buffer)
+
+    try:
+        resultado = model.transcribe(caminho_temp, language="pt")
+        texto = resultado["text"].strip()
+    finally:
+        if os.path.exists(caminho_temp):
+            os.remove(caminho_temp)
+    return { "texto" : texto }
+
